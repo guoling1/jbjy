38d666dcad9cf19401b5a4b9c2d6dcfeaf122504
guoling1 <646626114@qq.com>
2018-06-12 08:47:17 +0800

    修改

--- pages/newsDetail/newsDetail.js
+++ pages/newsDetail/newsDetail.js
@@ -1,11 +1,13 @@
 // pages/newsDetail/newsDetail.js
+var WxParse = require('../../wxParse/wxParse.js');
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    news:''
+    news:'',
+    content:''
   },
 
   /**
@@ -17,8 +19,11 @@ Page({
       url: 'https://jb.hdjincheng.cn/appbase/read.php?id='+options.id,
       success: function (res) {
         if (res.data.code == "100") {
-          res.data.data.picurl = res.data.data.picurl.replace(/http:\/\/localhost/, "https://jb.hdjincheng.cn")
+          console.log(res.data.data)
+          // res.data.data.picurl = res.data.data.picurl.replace(/http:\/\/localhost/, "https://jb.hdjincheng.cn")
+          WxParse.wxParse('content', 'html', res.data.data.content, _this, 15);
           _this.setData({ news: res.data.data })
+          
         }
       }
     })
